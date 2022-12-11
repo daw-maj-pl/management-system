a3578beef2f1496d7ba371c80c31fd2157143129
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-12-11 19:18:14 +0100

    Replace <a> with <Link>

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,16 @@
+import { Link } from 'react-router-dom';
 import logo from './assets/logo.png';
 
 export default function Header() {
   return (
     <nav className="navbar bg-light mb-4 p-0">
       <div className="container">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           <div className="d-flex">
             <img src={logo} alt="logo" />
             <div>Management System</div>
           </div>
-        </a>
+        </Link>
       </div>
     </nav>
   );
