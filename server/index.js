c3440a921b8004208f741a86773b130a77aa400d
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-12-14 22:24:49 +0100

    Prepare for deployment

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const cors = require('cors');
@@ -21,5 +22,13 @@ app.use(
     graphiql: process.env.NODE_ENV === 'development'
   })
 );
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/../client/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(port, console.log(`Server running on port ${port}`));
